780e50cb447ffbb370a459e8ea91135661e085ec
yassinekarami <yassine.karami@outlook.com>
2020-03-30 15:10:37 +0200

    init project

---
+++ Assets/Scripts/Player/Movements.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movements : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] float horizontal;
+    [SerializeField] bool grounded;
+    public float speed;
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.horizontal = Input.GetAxis("Horizontal");
+        this.rb.velocity = new Vector3(this.horizontal, 0, 0) * this.speed * Time.deltaTime;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(this.grounded) // saute
+            {
+                this.rb.AddForce(new Vector3(0, this.jumpForce, 0), ForceMode.Force);
+                this.grounded = false;
+            }
+            else // saute pas 
+            {
+
+            }
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            this.grounded = true;
+        }
+    }
+
+
+}
