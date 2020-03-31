2008378a4f83e412e457fbc3937ea42d6f5f1a8e
yassinekarami <yassine.karami@outlook.com>
2020-03-31 14:57:30 +0200

    ajout annimation shoot

--- Assets/Scripts/Ellen/Controller.cs
+++ Assets/Scripts/Ellen/Controller.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     Animator animator;
-    [SerializeField] float horizontal;
-    bool facingRight;
-    [SerializeField] int attack;
     Rigidbody rb;
+    GameObject staff;
+    GameObject pistol;
+
     public float jumpForce;
+    bool facingRight;
+    float shootTimer;
+
+    [SerializeField] int attack;
+    [SerializeField] int shoot;
+    [SerializeField] float horizontal;
+
+    // strings for input
+    string horizontalInput;
+    string attackInput;
+    string jumpInput;
+    string shootInput;
+
+    // strings for params
+    string jumpForceParam;
+    string runParam;
+    string attackParam;
+    string shootParam;
+    string isShootingParam;
 
     // Start is called before the first frame update
     void Start()
@@ -18,45 +37,76 @@ public class Controller : MonoBehaviour
         this.facingRight = true;
         this.attack = 0;
         this.rb = GetComponent<Rigidbody>();
-       
+        this.staff = GameObject.FindGameObjectWithTag("Staff");
+        this.staff.SetActive(false);
+        this.pistol = GameObject.FindGameObjectWithTag("Pistol");
+        this.pistol.SetActive(false);
+
+
+        this.horizontalInput = "Horizontal";
+        this.attackInput = "Attack";
+        this.jumpInput = "Jump";
+        this.shootInput = "Shoot";
+        this.jumpForceParam = "jumpForce";
+        this.runParam = "run";
+        this.attackParam = "attack";
+        this.shootParam = "shoot";
+        this.isShootingParam = "isShooting";
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.horizontal = Input.GetAxis("Horizontal");
+        this.horizontal = Input.GetAxis(this.horizontalInput);
      
-        if(this.facingRight && Input.GetAxis("Horizontal") < 0 || !this.facingRight && Input.GetAxis("Horizontal") > 0 )
+        if(this.facingRight && Input.GetAxis(this.horizontalInput) < 0 || !this.facingRight && Input.GetAxis(this.horizontalInput) > 0 )
         {
             Vector3 rotate = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
             rotate.z = rotate.z * -1;
             this.facingRight = !this.facingRight;
             gameObject.transform.localScale = rotate;
         }
-        if(Input.GetButtonDown("Horizontal"))
+        if(Input.GetButtonDown(this.horizontalInput))
         {
             this.attack = 0;
         }
         
-        if(Input.GetButtonDown("Attack"))
+        if(Input.GetButtonDown(this.attackInput))
         {
             this.attack++;
+            this.staff.SetActive(true);
         }
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown(this.jumpInput))
         {
             this.attack = 0;
             this.rb.AddForce(new Vector3(0, this.jumpForce, 0), ForceMode.Impulse);
         }
+        if (Input.GetButtonDown("Shoot")) // reinit timer 
+        {
+            this.shootTimer = 0f;
+                   
+            this.shoot++;
+            this.pistol.SetActive(true);
+        }
+        this.shootTimer += Time.fixedDeltaTime;
+        if (this.shootTimer > 2.0f) // fin du shoot
+        {
+            this.shoot = 0;
+            this.pistol.SetActive(false);
+        }
 
-        this.animator.SetFloat("jumpForce", this.rb.velocity.y);
-        this.animator.SetInteger("attack", this.attack);
-        this.animator.SetFloat("run", Mathf.Abs(this.horizontal));
+        this.animator.SetInteger(this.shootParam, this.shoot);
+        this.animator.SetBool(this.isShootingParam, Input.GetButtonDown("Shoot"));
+        this.animator.SetFloat(this.jumpForceParam, this.rb.velocity.y);
+        this.animator.SetInteger(this.attackParam, this.attack);
+        this.animator.SetFloat(this.runParam, Mathf.Abs(this.horizontal));
     }
-
-  
 
     private void endAttack()
     {
         this.attack = 0;
+        this.staff.SetActive(false);
     }
 }
